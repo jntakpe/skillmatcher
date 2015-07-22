d437837bd259140c2b53dcd1e349cc1a018f7b89
jntakpe <j.ntakpe@gmail.com>
2015-07-22 23:28:03 +0200

    Basic conf

---
+++ server/src/main/java/com/github/jntakpe/skillmatcher/config/UrlConstants.java
@@ -0,0 +1,15 @@
+package com.github.jntakpe.skillmatcher.config;
+
+/**
+ * Constantes liées aux URLs
+ *
+ * @author jntakpe
+ */
+public class UrlConstants {
+
+    public static final String BASE = "/api";
+
+    public static final String ACCOUNT = BASE + "/account";
+
+    public static final String API = BASE + "/api";
+}
